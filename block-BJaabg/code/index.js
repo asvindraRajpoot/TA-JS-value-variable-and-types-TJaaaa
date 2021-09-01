4919cf5c88b0139eca78bfd58420a741174aff9f
asvindra <aswin1399904@gmail.com>
2021-09-01 23:59:51 +0530

    Assignment: Level 3 of Loops and Decision

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -14,20 +14,19 @@ const ACCESSORY_PRICE = 9.99;
 var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
-for(amount;amount<=bank_balance;)
-
-{
-   
-    if(amount<=SPENDING_THRESHOLD)
-    {
-        amount=amount+PHONE_PRICE+ACCESSORY_PRICE;
+for (amount; amount <= bank_balance;) {
+    amount = amount + PHONE_PRICE;
+    if (amount <= SPENDING_THRESHOLD) {
+        amount = amount + ACCESSORY_PRICE;
     }
-    amount=amount+PHONE_PRICE;
-
-
-
 }
-amount+=amount*TAX_RATE;
+amount += amount * TAX_RATE;
 console.log(`$ ${amount}`);
+if (amount < bank_balance) {
+    alert(`you can afford the phone`);
+}
+else {
+    alert(`You can not afford the phone`);
+}
 
 // ⛑ Answer of the above will `$334.76`.
